10b038c2c4d218b36bbc63ecc5863c29a860e064
rsbrodov <rsbrodov@mail.ru>
2022-05-08 23:15:12 +0700

    Products/index and sorting

--- resources/js/Headers.vue
+++ resources/js/Headers.vue
@@ -7,8 +7,8 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
-       <li class="nav-item"><router-link class="nav-link" :to="{name: 'dishes'}">Блюда</router-link></li>
-       <li class="nav-item"><router-link class="nav-link" :to="{name: 'products'}">Продукты</router-link></li>
+        <li class="nav-item"><router-link class="nav-link" :to="{name: 'products'}">Продукты</router-link></li>
+        <li class="nav-item"><router-link class="nav-link" :to="{name: 'dishes'}">Блюда</router-link></li>
        <li class="nav-item"><router-link class="nav-link" :to="{name: 'menus'}">Мои меню</router-link></li>
        <li class="nav-item"><router-link class="nav-link" :to="{name: 'menus-dishes'}">Работа с меню</router-link></li>
        <li class="nav-item"><router-link class="nav-link" :to="{name: 'cabinet'}">Личный кабинет</router-link></li>
